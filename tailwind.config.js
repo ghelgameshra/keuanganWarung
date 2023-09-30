5389e6e0af426c0e381b1d4bb72cb4055bc389f2
ghelgameshra <ghelgameshra3347@gmail.com>
2023-09-30 19:36:54 +0700

    view: (membuat halaman login)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: ['class'],
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
-      extend: {},
+        extend: {
+            colors: {
+                'dark': '#0f172a',
+                'dark-secondary': '#1e293b',
+                'blue-main': '#35b1e8',
+                'secondary': '#505ac1',
+                'secondary-hover': '#3f46a8',
+            },
+            backgroundPosition: {
+                'pos-0': '0% 0%',
+                'pos-100': '100% 100%',
+            },
+            backgroundSize: {
+                'size-200': '200% 200%',
+            },
+        },
     },
     plugins: [],
-  }
+}
